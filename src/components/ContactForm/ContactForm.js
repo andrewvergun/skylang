9ff3d124bf8c61415ab2f9d784a15955d502afcb
Andrew Vergun <andrewvergun@gmail.com>
2025-07-17 19:39:00 +0200

    Add minor design changes

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,24 +1,29 @@
 import "@/styles/components/ContactForm.css";
+import {Sour_Gummy} from 'next/font/google';
 const fireworks= "/assets/fireworks.png";
 import Image from 'next/image';
 import HubspotForm from "@/components/HubspotForm/HubspotForm";
 
+const sourGummy = Sour_Gummy({
+    subsets: ['latin'],
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'], // customize as needed
+    style: ['normal', 'italic'],
+    display: 'swap',
+});
+
 const ContactForm = () => {
-
-
-
   return (
       <div id="contact-form-home">
         <h2 className="contact-form__title">Залишились питання?</h2>
         <div className="contact-form__container">
           <div className="contact-form__left">
             <h3>Напишіть нам!</h3>
-            <Image src={fireworks} width={204} height={204} alt="Феєрверки" className="contact-form__image"/>
+              <p style={{fontFamily: sourGummy.style.fontFamily}}>Cześć!</p>
           </div>
           <div className="contact-form__right">
-            <div className=".contact-form__form">
               <HubspotForm/>
-            </div>
+
+
 
           </div>
         </div>
