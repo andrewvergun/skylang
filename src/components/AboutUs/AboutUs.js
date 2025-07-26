6b7b03c4a1eb10891debf80c666644562643ee5f
Andrew Vergun <andrewvergun@gmail.com>
2025-07-26 11:13:42 +0200

    About Us Hero section ready

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,7 +1,23 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import "@/styles/components/AboutUs.css";
 
 const AboutUs = () => {
+    return (
+        <div className="about-us-page">
+            <div className="hero">
+                <h2 className='hero-title'>
+                    SkyLang - мовна школа, яка навчає результативно.
+                </h2>
+                <p className='hero-subtitle'>
+                    Польська та англійська з нуля до впевненого спілкування. Онлайн, з підтримкою, без нудної граматики.
+                </p>
+                <button className='hero-btn'>
+                    Переглянути курси
+                </button>
+            </div>
+        </div>
+    );
 
 }
 
