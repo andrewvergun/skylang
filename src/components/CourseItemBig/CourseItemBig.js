9449452ce6fa391dd85d6461fa5d46f27d2e68bb
Andrew Vergun <andrewvergun@gmail.com>
2025-07-30 17:30:42 +0200

    Fix UI issues

--- src/components/CourseItemBig/CourseItemBig.js
+++ src/components/CourseItemBig/CourseItemBig.js
@@ -28,15 +28,15 @@ const CourseItemBig = ({
                 </div>
                 <div className="course-item-big__bottom--right">
                     <div className="details">
-                        <div className="details-item">
+                        <div className="details-items">
                             <p>Тривалість: {courseDuration}</p>
                         </div>
 
-                        <div className="details-item">
+                        <div className="details-items">
                             <p>Кількість занять: {numberOfLessons}</p>
                         </div>
 
-                        <div className="details-item">
+                        <div className="details-items">
                             <p>Ціна: {price}</p>
                         </div>
                     </div>
